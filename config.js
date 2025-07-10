b57b1168b11237230f5340b3df1b9051daff7981
UR-2025 <unlockready.info@gmail.com>
2025-07-10 18:20:40 -0500

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// Pega tu clave de API aquí.
-// ¡IMPORTANTE! No compartas este archivo públicamente. Si usas Git, añade este archivo a tu .gitignore.
-const GEMINI_API_KEY = 'AIzaSyBfZhwndx9CmzbXfs9i1qIJE2fEsiW2aa8';
